a60f7a33b75c2598704fbe6801b8640698879f6d
Ambili das v s <ambili.kan@gmail.com>
2022-05-06 20:56:45 +0530

    Harmonic program is added

---
+++ HarmonicNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicNumber2
+{
+    internal class HarmonicNumber
+    {
+        double sum = 0.0;
+        public void findHarmonicNum()
+        {
+            Console.WriteLine("Find the nth Harmonic value");
+            Console.Write("\n\n");
+            Console.Write("Enter the number of terms: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n\n");
+
+            for (float i = 1; i <= n; i++)
+            {
+                sum += 1 / i;
+            }
+            Console.WriteLine("Sum of series up to {0} terms: {1}", n, sum);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HarmonicNumber2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HarmonicNumber myobj = new HarmonicNumber();
+            myobj.findHarmonicNum();
+        }
+    }
+}
